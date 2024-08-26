fcfc30a61c5e7aecfb7abe397e6040b88cf9de5f
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-08-26 16:49:14 +0900

    update:ホーム画面へのルートを追加しました

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route,} from "react-router-dom";
 import Login from "./page/Login";
- import Registration from "./page/Registration";
+import Registration from "./page/Registration";
+import Home from "./page/Home";
 
 const App: React.FC = () => {
   return(
     <Routes>
       <Route path="/" element={<Login/>} />
       <Route path="/Registration" element={<Registration/>}/>
+      <Route path="/Home" element={<Home/>}/>
     </Routes>
   );
 }
