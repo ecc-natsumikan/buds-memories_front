b7b500f0fb3cb4be589c236b60ee950e2e149aa4
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-08-27 19:51:14 +0900

    update:進捗度の表現するバーを追加しました

--- react-app/src/component/Quest_component.tsx
+++ react-app/src/component/Quest_component.tsx
@@ -1,17 +1,36 @@
 import './Quest_component.css';
-interface Quest_Props{
+import { Progress } from '@chakra-ui/react';
+
+interface Quest_Props {
     questtext: string;
-
+    progressvalue: number;
 }
-const Quest : React.FC<Quest_Props> =({questtext}) =>{
-    return(
-        <div>
-            <ul typeof="disc" className='Questlist'>
-                <li>{questtext}</li>
-                
-            </ul>
 
+const Quest: React.FC<Quest_Props> = ({ questtext, progressvalue }) => {
+    return (
+        <div className="QuestContainer">
+            <ul className="Questlist">
+                <li>{questtext}</li>
+            </ul>
+            <div className="ProgressWrapper">
+                <div className="ProgressLabels">
+                    <span>0%</span>
+                    <span>100%</span>
+                </div>
+                <Progress 
+                    value={progressvalue}
+                    width="30vw" 
+                    height="2vh"  
+                    backgroundColor="white"
+                    sx={{
+                        '& > div': {
+                            backgroundColor: '#F4853B',  // 進捗部分の色を指定
+                        },
+                    }}
+                />
+            </div>
         </div>
     );
 }
+
 export default Quest;
