680dcf9f3293db9431428b87b36b1f2c5a018161
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-08-30 01:51:52 +0900

    update:コードを修正しました

--- react-app/src/component/ProfileIcon .tsx
+++ react-app/src/component/ProfileIcon .tsx
@@ -9,10 +9,10 @@ const ProfileIcon: React.FC<ProfileIconProps> = ({ imageUrl }) => {
   return (
     <div
       style={{
-        display: 'flex',
+        
+        alignContent:'center',
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: '20px', // アイコンの下に余白を追加
         width: '100px', // 親要素の幅
         height: '100px', // 親要素の高さ
         borderRadius: '50%', // 親要素を円形にする
