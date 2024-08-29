dab6cc79acad8c490a53bfabb9bfcf84741eb39e
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-08-30 02:28:26 +0900

    :updateコードを更新しました

--- react-app/src/component/Side_navigation.tsx
+++ react-app/src/component/Side_navigation.tsx
@@ -6,12 +6,13 @@ interface Side_Navigation_Props {
     profileImageUrl: string; 
 }
 
-const Side_Navigation: React.FC<Side_Navigation_Props> = ({ profileImageUrl }) => {
+const Side_Navigation : React.FC<Side_Navigation_Props> =({profileImageUrl})=> {
     const menuItems = [
-        { text: "ホーム", path: "/Home" },
-        { text: "投稿", path: "/" },
-        { text: "プロフィール", path: "/" },
-        { text: "設定", path: "/" }
+        //表示したいタイトルとそれに該当したパス
+        { text: "ホーム" ,path:"/Home"},
+        { text: "投稿" , path:"/Post"},
+        {text:"プロフィール", path:"/Profile"},
+        {text:"設定", path:"/Setting"}
     ];
 
     return (
@@ -23,56 +24,52 @@ const Side_Navigation: React.FC<Side_Navigation_Props> = ({ profileImageUrl }) =
                 '& .MuiDrawer-paper': {
                     width: 170,
                     boxSizing: 'border-box',
-                    paddingTop: '50px', // サイドバーの上部に余白を追加
+                    paddingTop: '100px',
                     backgroundColor: '#F6E091',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center', // 横方向の中央揃え
+                    alignItems:'center'
                 },
             }}
         >
             <ProfileIcon imageUrl={profileImageUrl} />
-            <List sx={{ width: '100%' }}>
+            <List>
                 {menuItems.map((item, index) => (
-                    <ListItem button key={index}
+                    <ListItem  key={index}
                         component={Link}
                         to={item.path}
-                        sx={{
-                            marginBottom: '15px', // 余白
-                            justifyContent: 'center', // 横方向の中央揃え
-                        }}
+                        
                     >
                         <ListItemText
-                            sx={{
-                                position: 'relative', // 相対位置での調整を可能に
-                                '&:hover:before': {
-                                    content: '""', // before疑似要素の内容を定義
-                                    position: 'absolute',
-                                    left: 0,
-                                    right: 0,
-                                    bottom: '-2px', // テキストより2px下に配置
-                                    height: '3px', // 下線の高さ
-                                    backgroundColor: '#FFA76D', // 下線の色
-                                    transform: 'scaleX(1)',
-                                },
-                                '&:before': {
-                                    content: '""',
-                                    position: 'absolute',
-                                    left: 0,
-                                    right: 0,
-                                    bottom: '-2px', // 下線の位置
-                                    height: '3px', // 下線の高さ
-                                    backgroundColor: '#FFA76D',
-                                    transform: 'scaleX(0)',
-                                    transition: 'transform 0.2s ease', // アニメーション
-                                },
-                            }}
+                        sx={{
+                            position: 'relative', // 相対位置での調整を可能に
+                            '&:hover:before': {
+                                content: '""', // before疑似要素の内容を定義
+                                position: 'absolute',
+                                left: 0,
+                                right: 0,
+                                bottom: '-2px', // テキストより1px下に配置
+                                height: '3px', // 下線の高さ
+                                backgroundColor: '#FFA76D', // 下線の色
+                                transform: 'scaleX(1)',
+                            },
+                            '&:before': {
+                                content: '""',
+                                position: 'absolute',
+                                left: 0,
+                                right: 0,
+                                bottom: '-2px', // 下線の位置
+                                height: '3px', // 下線の高さ
+                                backgroundColor: '#FFA76D',
+                                transform: 'scaleX(0)',
+                                transition: 'transform 0.2s ease', // アニメーション
+                            },
+                        }}
                             primary={item.text}
                             primaryTypographyProps={{
                                 fontFamily: 'Zen Maru Gothic, sans-serif',  // フォント設定
                                 fontWeight: 'bold',
-                                fontSize: '20px',
-                                textAlign: 'center',
+                                fontSize:'20px',
+                                textAlign:'center',
+                                color:'black'
                             }}
                         />
                     </ListItem>
