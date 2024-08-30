db407f341e782b25e5064a6961fa330514512cfc
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-08-30 14:57:01 +0900

    update:タグをタップしたら、画像ととタグの名前を表示するようにしました

--- react-app/src/component/Tag_component.tsx
+++ react-app/src/component/Tag_component.tsx
@@ -1,12 +1,23 @@
+import React from 'react';
 import './Tag_component.css';
-interface TagProps{
-    text:string;
+import { useNavigate } from 'react-router-dom';
+interface TagProps {
+    text: string;
+    img: string;
 }
-const Tag : React.FC<TagProps> =({text}) =>{
-    return(
-        <div className="tag">
+
+const Tag: React.FC<TagProps> = ({ text,img}) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/Growth',{ state:{tagtitle:text,tagimage:img}}); // /Growthに遷移タグの名前を渡す
+    };
+
+    return (
+        <button className="tag_component" onClick={handleClick}>
             {text}
-        </div>
+        </button>
     );
 }
+
 export default Tag;
